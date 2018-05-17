88343d9e183f612ab80938db959402af2587b15a
william ramos <wulliamcostaramos@gmail.com>
2018-05-16 21:55:46 -0300

    documentação da classe Insert

--- database/Insert.php
+++ database/Insert.php
@@ -4,7 +4,7 @@
  * <b>Insert</b>
  * Classe responsavel por realizar cadastro genericos no banco de dados.
  *
- * @author William Ramos
+ * @copyright (c) 2018, William Ramos
  */
 class Insert {
 
@@ -22,29 +22,51 @@ class Insert {
         $this->insert($tabela, $dados);
     }
 
+    /**
+     * Metodo responsavel por realizar o cadastro
+     * Informe uma tabela e um Array de dados Associativo  (nome da coluna => dado a ser Inserido).
+     * @param type $tabela
+     * @param array $dados
+     */
     public function insert($tabela, array $dados) {
 
         $this->tabela = (string) $tabela;
         $this->dados = $dados;
         $this->getSintax();
     }
-    
+
+    /**
+     * <b>getResult</b>
+     * Metodo responsavel por retornar o ultimo resultado inserido no banco.
+     * 
+     */
     public function getResult() {
         return $this->result;
-        
     }
 
+    /**
+     * <b>connect</b>
+     * Metodo por realizar a conexao com o banco de dados.
+     */
     private function connect() {
         $this->con = Connection::getConnection();
         $this->create = $this->con->prepare($this->create);
     }
 
+    /**
+     * <b>getSintax</b>
+     * Metodo responsavel por montar a query de Inserção no banco de Dados.
+     */
     private function getSintax() {
         $colunas = implode(', ', array_keys($this->dados));
         $valores = ':' . implode(', :', array_keys($this->dados));
         $this->create = "INSERT INTO {$this->tabela} ({$colunas}) VALUES ({$valores})";
     }
 
+    /**
+     * <b>execute</b>
+     * Metodo responsavel por executar a query no banco de dados.
+     */
     private function execute() {
 
         $this->connect();
@@ -56,6 +78,5 @@ class Insert {
             echo $e->getMessage();
         }
     }
-    
 
 }
